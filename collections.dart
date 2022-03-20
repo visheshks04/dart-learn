5eae57f7ef8c486b5c29a0e053a99541393e1d23
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2022-03-20 23:23:24 +0530

    until collections functions and classes

--- collections.dart
+++ collections.dart
@@ -1,1 +1,39 @@
+void main() {
+  List nums = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    'vishesh'
+  ]; // can be dynamically typed/heterogenous types like now or List<int> to make it statically typed
 
+  nums.forEach((element) {
+    print(element);
+  });
+
+  print("Length is: " + nums.length.toString());
+
+  Set newSet = {
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6
+  }; // duplicated are ignored
+
+  newSet.forEach((n) => print(n));
+
+  Map dictionary = {'one': 1, 'two': 2, 'three': 3}; // like python's dictionary
+
+  print(dictionary['two']);
+}
---
+++ functions.dart
@@ -0,0 +1,29 @@
+// returnType name(arge) { body }
+
+int sum(List<int> l) {
+  int s = 0;
+  for (var num in l) {
+    s += num;
+  }
+  return s;
+}
+
+dynamic add({var num1, var num2}) {
+  return num1 + num2;
+} // named parameter
+
+void main() {
+  List<int> l = [1, 2, 3];
+  print(sum(l));
+
+  dynamic square =
+      (n) => n * n; // arrow function like in JS and lambda in python
+
+  print(square(10));
+
+  // named parameters
+  print(add(num1: 10, num2: 23));
+}
+
+
+// Classes are exactly same as C++ (; termination is not used)
